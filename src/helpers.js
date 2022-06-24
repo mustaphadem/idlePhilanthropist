9f801c90fe1ee7a707cd937af75f69d8dfd18bd7
Mustafa Taha Demirci <65232645+mustaphadem@users.noreply.github.com>
2022-06-24 14:29:46 -0700

    Initial commit

---
+++ src/helpers.js
@@ -0,0 +1,8 @@
+import numeral from "numeral";
+
+export const format = (value) => {
+  if (value > 999999) return numeral(value).format("$ 0.00a");
+  if (value > 999) return numeral(value).format("$ 0,");
+  if (value > 99) return numeral(value).format("$ 0a");
+  else return numeral(value).format("$ 0.00a");
+};
